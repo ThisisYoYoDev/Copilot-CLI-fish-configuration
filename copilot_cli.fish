f741dfca21822419a9c344d7ec9516465f7bac5a
Yoel Edery <71037962+ThisisYoYoDev@users.noreply.github.com>
2023-03-28 22:23:07 +0200

    Create copilot_cli.fish

---
+++ copilot_cli.fish
@@ -0,0 +1,69 @@
+# Fish configuration for Copilot-CLI
+
+set copilot_cli_path (which github-copilot-cli)
+
+# check if copilot is installed
+if test -z "$copilot_cli_path"
+  echo "Copilot CLI not found. Please install it from https://www.npmjs.com/package/@githubnext/github-copilot-cli"
+  exit 1
+end
+
+function copilot_what-the-shell --description 'Copilot CLI: What the shell ?'
+  set TMPFILE (mktemp)
+  trap 'rm -f $TMPFILE' EXIT;
+
+  if $copilot_cli_path what-the-shell "$argv" --shellout $TMPFILE
+
+    if [ -e "$TMPFILE" ]
+      set FIXED_CMD (cat $TMPFILE)
+      eval "$FIXED_CMD"
+    else
+      echo "Apologies! Extracting command failed"
+    end
+
+  else
+    return 1
+  end
+
+end
+
+function copilot_git-assist --description 'Copilot CLI: Git assist'
+  set TMPFILE (mktemp)
+  trap 'rm -f $TMPFILE' EXIT;
+
+  if $copilot_cli_path git-assist "$argv" --shellout $TMPFILE
+
+    if [ -e "$TMPFILE" ]
+      set FIXED_CMD (cat $TMPFILE)
+      eval "$FIXED_CMD"
+    else
+      echo "Apologies! Extracting command failed"
+    end
+
+  else
+    return 1
+  end
+
+end
+
+function copilot_gh-assist --description 'Copilot CLI: Github assist'
+  set TMPFILE (mktemp)
+  trap 'rm -f $TMPFILE' EXIT;
+  if $copilot_cli_path gh-assist "$argv" --shellout $TMPFILE
+
+    if [ -e "$TMPFILE" ]
+      set FIXED_CMD (cat $TMPFILE)
+      eval "$FIXED_CMD"
+    else
+      echo "Apologies! Extracting command failed"
+    end
+
+  else
+    return 1
+  end
+end
+
+alias '??'='copilot_what-the-shell';
+alias "git\?"='copilot_git-assist';
+alias 'gh\?'='copilot_gh-assist';
+alias 'wts'='copilot_what-the-shell';
