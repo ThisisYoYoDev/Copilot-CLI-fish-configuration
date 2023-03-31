6fff60182bdbe180909e14864329e0413c701e6e
ThisisYoYoDev <yoel.edery@epitech.eu>
2023-03-31 19:58:58 +0200

    add copilot_cli.fish

--- copilot_cli.fish
+++ copilot_cli.fish
@@ -1,4 +1,3 @@
-
 # Fish configuration for Copilot-CLI
 
 set copilot_cli_path (which github-copilot-cli)
@@ -9,69 +8,33 @@ if test -z "$copilot_cli_path"
   exit 1
 end
 
-function copilot_what-the-shell --description 'Copilot CLI: What the shell ?'
+function copilot_assist --description 'Copilot CLI: Assist'
   set TMPFILE (mktemp)
   trap 'rm -f $TMPFILE' EXIT
 
-  if $copilot_cli_path what-the-shell "$argv" --shellout $TMPFILE
-
+  if $copilot_cli_path $argv[1] (echo $argv[2..-1]) --shellout $TMPFILE
     if [ -e "$TMPFILE" ]
-      set FIXED_CMD (cat $TMPFILE)
-      eval "$FIXED_CMD"
+      eval (cat $TMPFILE | sed ':a;N;$!ba;s/\n/ ; /g')
+      if test $status -ne 0
+        echo "Apologies! The command failed, log is available at $TMPFILE"
+      end
     else
       echo "Apologies! Extracting command failed"
     end
-
-  else
-    return 1
-  end
-
-end
-
-function copilot_git-assist --description 'Copilot CLI: Git assist'
-  set TMPFILE (mktemp)
-  trap 'rm -f $TMPFILE' EXIT
-
-  if $copilot_cli_path git-assist "$argv" --shellout $TMPFILE
-
-    if [ -e "$TMPFILE" ]
-      set FIXED_CMD (cat $TMPFILE)
-      eval "$FIXED_CMD"
-    else
-      echo "Apologies! Extracting command failed"
-    end
-
-  else
-    return 1
-  end
-
-end
-
-function copilot_gh-assist --description 'Copilot CLI: Github assist'
-  set TMPFILE (mktemp)
-  trap 'rm -f $TMPFILE' EXIT
-  if $copilot_cli_path gh-assist "$argv" --shellout $TMPFILE
-
-    if [ -e "$TMPFILE" ]
-      set FIXED_CMD (cat $TMPFILE)
-      eval "$FIXED_CMD"
-    else
-      echo "Apologies! Extracting command failed"
-    end
-
   else
     return 1
   end
 end
+
 
 # Aliases for what-the-shell
-alias '!!'='copilot_what-the-shell'
-alias 'wts'='copilot_what-the-shell'
+alias '!!'='copilot_assist what-the-shell'
+alias 'wts'='copilot_assist what-the-shell'
 
 # Aliases for git-assist
-alias 'git!'='copilot_git-assist'
-alias 'ga'='copilot_git-assist'
+alias 'git!'='copilot_assist git-assist'
+alias 'ga'='copilot_assist git-assist'
 
 # Aliases for gh-assist
-alias 'gh!'='copilot_gh-assist'
-alias 'gha'='copilot_gh-assist'
+alias 'gh!'='copilot_assist gh-assist'
+alias 'gha'='copilot_assist gh-assist'
